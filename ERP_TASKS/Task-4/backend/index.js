d324781113e5c8da2b4941f664587ec3c1840353
Yeluri Bala Siva Durga Aswith <136434154+aswithyeluri@users.noreply.github.com>
2025-04-16 10:46:13 +0530

    Update index.js

--- ERP_TASKS/Task-4/backend/index.js
+++ ERP_TASKS/Task-4/backend/index.js
@@ -25,10 +25,9 @@ const port = 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
-
 mongoose.connect(
-    "mongodb+srv://vignanuser:QasQik7m.m2C5Lw@vignanuniversitycluster.tr5ru.mongodb.net/211FA04369",
-    { useNewUrlParser: true, useUnifiedTopology: true }
+    //mongodb cluster
+    '', {useNewUrlParser: true, useUnifiedTopology: true}
 );
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
